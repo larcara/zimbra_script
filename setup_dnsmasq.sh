ef9c734da0e5be02821e149b67ce7da018d22081
Luca Arcara <larcara@gmail.com>
2017-05-22 10:32:30 +0200

    update setup_dnsmasq
    add zextras and zimbra download

--- setup_dnsmasq.sh
+++ setup_dnsmasq.sh
@@ -1,6 +1,4 @@
-root@zimbra725:~# cat setup_dnsmasq.sh
 #/bin/bash
-
 
 if [ "$(whoami)" != 'root' ]; then
         echo $"You have no permission to run $0 as non-root user. Use sudo"
@@ -18,3 +16,13 @@ echo "mx-host=zimbra.local, $HOSTNAME.zimbra.local, 5" >> $DNSMASQ_CONF
 echo "mx-host=$HOSTNAME.zimbra.local, $HOSTNAME.zimbra.local, 5" >> $DNSMASQ_CONF
 echo "listen-address=127.0.0.1" >> $DNSMASQ_CONF
 service dnsmasq restart
+mkdir -p /opt/sw
+cd /opt/sw
+wget http://download.zextras.com/zextras_suite-latest.tgz &
+case `lsb_release -rs` in
+"16.04") wget https://files.zimbra.com/downloads/8.7.9_GA/zcs-8.7.9_GA_1794.UBUNTU16_64.20170505054622.tgz;;
+"14.04") wget https://files.zimbra.com/downloads/8.7.9_GA/zcs-8.7.9_GA_1794.UBUNTU14_64.20170505054622.tgz;;
+"12.04") wget https://files.zimbra.com/downloads/8.7.9_GA/zcs-8.7.9_GA_1794.UBUNTU12_64.20170505054622.tgz;;
+esac
+
+
